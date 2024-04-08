83f0827e6fde7e493ff00b12f7d2e6f10986b689
jiohjung98 <ckaquz98@naver.com>
2024-04-08 18:29:46 +0900

    design: 셀렉트 박스 생성

--- pages/project.js
+++ pages/project.js
@@ -8,11 +8,10 @@ import Plan from '@/components/project/plan/plan';
 export default function Project() {
     const [category, setCategory] = useState('all');
 
-    const handleCategoryChange = (newCategory) => {
-        setCategory(newCategory);
+    const handleCategoryChange = (event) => {
+        setCategory(event.target.value);
     };
 
-    // 카테고리에 따라 렌더링할 컴포넌트를 결정하는 함수
     const renderContentByCategory = () => {
         switch (category) {
             case 'all':
@@ -34,12 +33,20 @@ export default function Project() {
                 {/* <link rel='icon' href='/favicon.ico'/> */}
             </Head>
             <div className="container mx-auto flex px-5 py-5 md:flex-row flex-col items-center">
-                <button onClick={() => handleCategoryChange('all')}>전체</button>
-                <button onClick={() => handleCategoryChange('development')}>개발</button>
-                <button onClick={() => handleCategoryChange('planning')}>기획</button>
+                <select className="category-select" value={category} onChange={handleCategoryChange}>
+                    <option value="all">전체</option>
+                    <option value="development">개발</option>
+                    <option value="planning">기획</option>
+                </select>
             </div>
-            {/* 카테고리에 따라 다른 컴포넌트를 렌더링 */}
             {renderContentByCategory()}
+            <style jsx>{`
+                .category-select {
+                    width: 60px; 
+                    height: 40px; 
+                    font-size: 18px;
+                }
+            `}</style>
         </Layout>
     )
 }
