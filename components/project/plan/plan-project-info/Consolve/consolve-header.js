fee3eedcff04adf81d3e7662f06922756f1d8d5b
jiohjung98 <ckaquz98@naver.com>
2024-04-08 18:08:20 +0900

    design: 텍스트 잘못입력 변경

--- components/project/plan/plan-project-info/Consolve/consolve-header.js
+++ components/project/plan/plan-project-info/Consolve/consolve-header.js
@@ -5,7 +5,7 @@ export default function ConsolveHeader() {
         <section className="container mx-auto flex">
             <a className="container mx-auto flex md:flex-row flex-col items-center" href="https://github.com/jiohjung98/Indiconnect">
                 <Image src='/Consolve-logo.png' alt='Consolve-logo'width="60" height="60"></Image>
-                <div className="ml-5 text-2xl font-semibold">Consolve - 뮤지션과 업주, 관객 연결 플랫폼</div>
+                <div className="ml-5 text-2xl font-semibold">Consolve - 콘텐츠 자격 증명 플랫폼</div>
             </a>
         </section>
     )
--- components/project/plan/plan-project-info/IndiConnects/indi-header.js
+++ components/project/plan/plan-project-info/IndiConnects/indi-header.js
@@ -1,11 +1,11 @@
 import Image from 'next/image'
 
-export default function ConsolveHeader() {
+export default function IndiHeader() {
     return (
         <section className="container mx-auto flex">
             <a className="container mx-auto flex md:flex-row flex-col items-center" href="https://github.com/jiohjung98/Consolveconnect">
-                <Image src='/Consolve-logo.png' alt='Consolve-logo'width="60" height="60"></Image>
-                <div className="ml-5 text-2xl font-semibold">Consolve - 뮤지션과 업주, 관객 연결 플랫폼</div>
+                <Image src='/Indi-logo.png' alt='Indi-logo'width="60" height="60"></Image>
+                <div className="ml-5 text-2xl font-semibold">IndiConnects - 뮤지션과 업주, 관객 연결 플랫폼</div>
             </a>
         </section>
     )
