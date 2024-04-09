fdae7a273490dedca2e202e189f666368253f87e
jiohjung98 <ckaquz98@naver.com>
2024-04-09 16:15:58 +0900

    design: 이미지 hover 값 적용

--- components/awards/awards.js
+++ components/awards/awards.js
@@ -8,7 +8,7 @@ export default function Awards() {
                 <div className="ml-5 text-xl mb-3 font-semibold">최우수상 수상</div>
                 <div className="ml-5 text-xl mb-2">한양대학교 ERICA 재학생들이 모여 무박 2일 간 아이디어를 도출하고 구체화하여 사업계획서를 작성하고 발표하는 대회</div>
                 <div className="ml-5 text-xl">한양대학교 ERICA 창업동아리 '콘솔브(콘텐츠 자격 증명 플랫폼)' 팀 프로젝트</div>
-                <img className='mb-20 mt-10 ml-5' src='/Software-up-8.png' alt='software-up 8' width="300" height="500" />
+                <img className='mb-20 mt-10 ml-5 hover:scale-105 transition-transform' src='/Software-up-8.png' alt='software-up 8' width="300" height="500" />
             </section>
             <hr className="w-10/12 mx-auto my-10 border-gray-300"/> 
             <section className="container mx-auto flex-row md:flex-row flex-col items-center">
@@ -16,7 +16,7 @@ export default function Awards() {
                 <div className="ml-5 text-xl mb-3 font-semibold">대상 수상</div>
                 <div className="ml-5 text-xl mb-2">한양대학교 ERICA 재학생들이 모여 무박 2일 간 아이디어를 도출하고 구체화하여 사업계획서를 작성하고 발표하는 대회</div>
                 <div className="ml-5 text-xl">한양대학교 ERICA 창업동아리 '티미룸(웹툰 공동 드로잉 플랫폼)' 팀 프로젝트</div>
-                <img className='mb-20 mt-10 ml-5' src='/Software-up-10.png' alt='software-up 10' width="300" height="500" />
+                <img className='mb-20 mt-10 ml-5 hover:scale-105 transition-transform' src='/Software-up-10.png' alt='software-up 10' width="300" height="500" />
             </section>
             <hr className="w-10/12 mx-auto my-10 border-gray-300"/> 
             <section className="container mx-auto flex-row md:flex-row flex-col items-center">
@@ -24,7 +24,7 @@ export default function Awards() {
                 <div className="ml-5 text-xl mb-3 font-semibold">대상 수상</div>
                 <div className="ml-5 text-xl mb-2">한양대학교 ERICA 재학생들이 모여 3개월 간 아이디어를 도출하고 구체화하여 사업계획서를 작성하고 개발 결과물을 발표하는 대회</div>
                 <div className="ml-5 text-xl">한양대학교 ERICA 창업동아리 ‘졸업시켜조(뮤지션과 업주, 관객 연결 플랫폼)’ 팀 프로젝트</div>
-                <img className='mb-20 mt-10 ml-5' src='/makerton-5.png' alt='makerton 5' width="300" height="500" />
+                <img className='mb-20 mt-10 ml-5 hover:scale-105 transition-transform' src='/makerton-5.png' alt='makerton 5' width="300" height="500" />
             </section>
         </section>
     )
--- components/layout.js
+++ components/layout.js
@@ -3,10 +3,29 @@ import Header from "./header";
 
 export default function Layout({ children }) {
     return (
-        <div className="bg-primary">
+        <div className="layout">
             <Header/>
-            <div>{children}</div>
+            <div className="content">{children}</div>
             <Footer/>
+            <style jsx global>{`
+                body {
+                    margin: 0;
+                    padding: 0;
+                    font-family: -apple-system, BlinkMacSystemFont, sans-serif;
+                    overflow: hidden;
+                }
+                .layout {
+                    display: flex;
+                    flex-direction: column;
+                    height: 100vh;
+                    overflow: auto;
+                }
+                .content {
+                    flex: 1;
+                    overflow-y: auto;
+                    padding: 20px; /* 내용에 대한 여백 설정 */
+                }
+            `}</style>
         </div>
-    )
+    );
 }
