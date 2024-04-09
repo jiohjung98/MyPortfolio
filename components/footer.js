60fe8372a2b738e2a59322b0bdd543b7b77784a0
jiohjung98 <ckaquz98@naver.com>
2024-04-09 17:07:51 +0900

    design: 푸터 텍스트 위치 조정 및 탭 아이콘 지정

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,7 @@ export default function Footer() {
         <footer className="body-font">
             <div className="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col">
                 <a href="/" className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-                    <h2 className="ml-3 text-xl">Jioh Jung Portfolio</h2>
+                    <h2 className="text-xl">Jioh Jung Portfolio</h2>
                 </a>
                 <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
                     <a href="https://github.com/jiohjung98" className="ml-3">
--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link rel="icon" href="/Main-logo.png" type="image/png" />
       <body>
         <Main />
         <NextScript />
