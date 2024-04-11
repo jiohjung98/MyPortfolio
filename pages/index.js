10e2b61c58bad202ab0a949a4dcc0075e0b930ef
jiohjung98 <ckaquz98@naver.com>
2024-04-11 17:37:11 +0900

    fix: 토글 이미지 미리 업로드

--- pages/index.js
+++ pages/index.js
@@ -54,6 +54,21 @@ export default function Home() {
     setShowImages2(!showImages2);
   };
 
+  const preloadImages = () => {
+    const images = ['/Fastcampus-course-completion.png', '/Fastcampus-Group-Leader.png', '/Fastcampus-Leader.png'];
+    images.forEach((image) => {
+        const img = new Image();
+        img.src = image;
+    });
+};
+
+  const preloadImages2 = () => {
+    const image = '/Umc-3rd.png'; 
+    const img = new Image();
+    img.src = image;
+  };
+
+
   useEffect(() => {
     if (completedThird) {
       setShowButton(true);
@@ -280,18 +295,18 @@ export default function Home() {
             <div className="md:flex-grow">
               <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">패스트캠퍼스 x 야놀자 웹 프론트엔드 부트캠프 1기</h2>
               <p className="leading-relaxed text-medium">경쟁력 있는 개발자가 되기 위해 웹 프론트엔드 개발 능력도 갖춘 개발자가 되고 싶다는 생각으로 패스트캠퍼스 x 야놀자 웹 프론트엔드 부트캠프 1기 교육과정을 수료했습니다. Js, React, Next 등을 배우며 웹 개발에 필요한 기술들을 배우고 실제 기업 제시 프로젝트를 통해 프로그래밍 능력을 향상시켰습니다. 또한 해당 교육에서는 그룹장 및 전체 리더로 활동하면서 커뮤니케이션 능력과 리더십을 기를 수 있었습니다.</p>
-              <h2 onClick={toggleImages} className="cursor-pointer mt-4">
+              <h2 onClick={() => {toggleImages(); preloadImages();}} className="cursor-pointer mt-4">
                 {showImages ? "증빙자료 ▲" : "증빙자료 ▼"}
-              </h2>
-              <section className='flex flex-col xl:flex-row'>
+            </h2>
+            <section className='flex flex-col xl:flex-row'>
                 {showImages && (
-                  <div className="flex flex-col xl:flex-row">
-                    <img className="my-10 mr-10" src="/Fastcampus-course-completion.png" alt="Fastcampus-course-completion" width="300" height="500" />
-                    <img className="my-10 mr-10" src="/Fastcampus-Group-Leader.png" alt="Fastcampus-Group-Leader" width="300" height="500" />
-                    <img className="my-10" src="/Fastcampus-Leader.png" alt="Fastcampus-Leader" width="300" height="500" />
-                  </div>
+                    <div className="flex flex-col xl:flex-row">
+                        <img className="my-10 mr-10" src="/Fastcampus-course-completion.png" alt="Fastcampus-course-completion" width="300" height="500" />
+                        <img className="my-10 mr-10" src="/Fastcampus-Group-Leader.png" alt="Fastcampus-Group-Leader" width="300" height="500" />
+                        <img className="my-10" src="/Fastcampus-Leader.png" alt="Fastcampus-Leader" width="300" height="500" />
+                    </div>
                 )}
-              </section>
+            </section>
             </div>
           </div>
           <div className="py-8 flex flex-wrap md:flex-nowrap">
@@ -302,7 +317,7 @@ export default function Home() {
             <div className="md:flex-grow">
               <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">IT 연합 동아리 University MakeUs Challenge 3rd Android 파트</h2>
               <p className="leading-relaxed text-medium">IT 연합 동아리 UMC 에서 안드로이드 파트 구성원으로 활동하며 안드로이드 생태계에 대해 배우고 Kotlin 언어를 학습하며 안드로이드 어플리케이션 개발 프로젝트에 참여하였습니다. 이 활동을 통해 협업 방법을 배우고 프론트엔드 개발자가 되고자 하는 꿈이 생겼습니다.</p>
-              <h2 onClick={toggleImages2} className="cursor-pointer mt-4">
+              <h2 onClick={() => {toggleImages2(); preloadImages2();}} className="cursor-pointer mt-4">
                 {showImages2 ? "증빙자료 ▲" : "증빙자료 ▼"}
               </h2>
               {showImages2 && (
