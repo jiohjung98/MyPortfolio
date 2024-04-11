593def9ea446fe5ab07b3489e63bae8c67f2d033
jiohjung98 <ckaquz98@naver.com>
2024-04-11 16:15:25 +0900

    design: 프로젝트 헤더 언더라인

--- components/project/develop/develop-project-info/Coolpeace/coolPeace-header.js
+++ components/project/develop/develop-project-info/Coolpeace/coolPeace-header.js
@@ -5,7 +5,7 @@ export default function CoolPeaceHeader() {
         <section className="container mx-auto flex">
             <a className="container mx-auto flex md:flex-row flex-col items-center" href="https://github.com/CoolPeace-yanolza">
                 <Image src='/coolPeace-logo.png' alt='Coolpeace-logo'width="50" height="50"></Image>
-                <h1 className="ml-5 text-3xl font-semibold" href="https://github.com/CoolPeace-yanolza">Coolpeace - B2B 셀프 쿠폰 백오피스</h1>
+                <h1 className="ml-5 text-3xl font-semibold hover:underline" href="https://github.com/CoolPeace-yanolza">Coolpeace - B2B 셀프 쿠폰 백오피스</h1>
             </a>
         </section>
     )
